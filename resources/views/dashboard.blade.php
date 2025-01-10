caa9163c60be23d97c90b4e276c22e7a82f8cab7
Lucas Gazetta <lucas.gazetta@perplan.com.br>
2025-01-10 13:46:52 -0300

    ajuste do logo

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,32 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('contrato') }}
         </h2>
     </x-slot>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <p>Texto</p>
+                        <H1>Contrato de Namoro</H1>
+                        <p>Lav</p>
+                        <p>quando penso em ti,</p>
+                        <p>penso em bondade</p>
+                        <p>penso em amor</p>
+                        <p>penso em carinho</p>
+                        <p>penso em compaixao</p>
+                        <p>pesno em companherismo</p>
+                        <p>penso em lealdade</p>
+                        <p>penso em alguem que eu tive a sorte de cruzar o caminho.</p>
+                        <p>por muitas vezes me peguei pensando em porque? procurando motivos que justificassem a minha paixão por vc</p>
+                        <p>mas não entcontrei um unico especifico, acho que porque me apaixonei por você de corpo e alma.</p>
+                        <p>Sinto um conforto quando estou com você, uma paz, uma segurança que só tem uma explicação, AMOR.</p>
+                        <p>Sempre que eu vivencio algo legal, penso que seria melhor se pudesse dividir esse momento com você.</p>
+                        <p>Nossa quimica é inexplicavel, unidos de mente, corpo e alma.</p>
+                        <p>Chegou o momento de fazermos uma escolha, e eu ja fiz a minha, agora quero saber de você:</p>
+                        <p>Gostaria de dividir a minha vida com você, oque acha ?</p>
                         <div>
                             <a href="/index">
-                                <x-primary-button>SIM</x-primary-button>
+                                <x-primary-button>Aceito</x-primary-button>
                             </a>
                         </div>
                         <div class="mt-4">
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,13 @@
 <x-guest-layout>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <i<i class="bi bi-balloon-heart">TEXTO</i>
+                    <i class="bi bi-envelope-paper-heart"></i>
                 </div>
             </div>
         </div>
