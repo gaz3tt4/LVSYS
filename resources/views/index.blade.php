d277db6aa68bc370ad9e0f037b8cbcc78bd25ac3
Lucas Gazetta <lucas.gazetta@perplan.com.br>
2025-01-23 13:40:18 -0300

    alteração Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1></h1>
+                    <h1>
+                        Vamos comprar as alianças??
+                    </h1>
+                    {{-- <img src="{{ asset }}" width="200"/>  coloacar imagem de coração fofinha--}}
                 </div>
             </div>
         </div>
