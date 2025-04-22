96b8ad3c6f01bf28e30ddd3c0bbf455fa894ff9e
Lucas Gazetta <lucas.gazetta@perplan.com.br>
2025-04-22 11:59:13 -0300

    add object

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,16 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
+   return viw ('index');
+})->middleware(['auth', 'verified'])->name('home');
+
+Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/index', [ProfileController::class, 'index'])->name('index');
-});
+// Route::middleware('auth')->group(function () {
+//     Route::get('/index', [ProfileController::class, 'index'])->name('index');
+// });
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
