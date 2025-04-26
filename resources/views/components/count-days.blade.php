1d6adac26f5376bc4e5f8957afc88ec560ec8dab
lucasGazetta <lucas@gazetta>
2025-04-26 19:34:54 -0300

    ajuste timer

--- resources/views/components/count-days.blade.php
+++ resources/views/components/count-days.blade.php
@@ -1,27 +1,46 @@
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" id="bootstrap-css">
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+<link href='https://fonts.googleapis.com/css?family=Agbalumo' rel='stylesheet'>
+  <style>
+        h1 {
+          font-family: 'Agbalumo';font-size: 40px; text-align: center
+        }
+        h2{
+          font-family: 'Agbalumo';font-size: 15px; text-align: center
+        }
+        </style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flipclock/0.7.8/flipclock.js"></script>
 <!------ Include the above in your HEAD tag ---------->
+
+<div>
+    <h1>Juntos a exatos:</h1>
+</div>
 
 <div class="container">
    <div class="row">
       <div class="col-lg-6 offset-3">
          <div class="timer">
-            <div>
-               <span class="days" id="day"></span>
-               <div class="smalltext">Days</div>
+            <div >
+                <div class="smalltext">Days:
+                    <span class="days" id="day"></span>
+                </div>
             </div>
             <div>
-               <span class="hours" id="hour"></span>
-               <div class="smalltext">Hours</div>
+                <div class="smalltext">Hours:
+                    <span class="hours" id="hour"></span>
+                </div>
             </div>
             <div>
-               <span class="minutes" id="minute"></span>
-               <div class="smalltext">Minutes</div>
+                <div class="smalltext">Minutes:
+                    <span class="minutes" id="minute"></span>
+                </div>
             </div>
             <div>
-               <span class="seconds" id="second"></span>
-               <div class="smalltext">Seconds</div>
+                <div class="smalltext">Seconds:
+                    <span class="seconds" id="second"></span>
+                </div>
             </div>
             <p id="time-up"></p>
          </div>
@@ -29,10 +48,10 @@
    </div>
 </div>
 <script type="text/javascript">
-var deadline = new Date("January 31, 2025 20:30:25").getTime();
+var deadline = new Date("January 12, 2025 01:30:25").getTime();
 var x = setInterval(function() {
    var currentTime = new Date().getTime();
-   var t = deadline - currentTime;
+   var t = currentTime - deadline;
    var days = Math.floor(t / (1000 * 60 * 60 * 24));
    var hours = Math.floor((t%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
    var minutes = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="mt-4">
-                        <x-timer-clock></x-timer-clock>
+                        <x-count-days></x-count-days>
                     </div>
                 </div>
             </div>
